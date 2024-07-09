d63b5a4504b1e6ea61bfeb708ae447d06a6f5639
Satoru Nakamura <na.kamura.1263@gmail.com>
2024-07-09 18:12:41 +0900

    feat: add time param

--- demo/app.js
+++ demo/app.js
@@ -12,15 +12,17 @@ import AutoAdvanceToggle from '@Components/AutoAdvanceToggle/AutoAdvanceToggle';
 import MarkersDisplay from '@Components/MarkersDisplay/MarkersDisplay';
 import './app.scss';
 
-const App = ({ manifestURL }) => {
+const App = ({ manifestURL, startCanvasTime }) => {
   const [userURL, setUserURL] = React.useState(manifestURL);
   const [manifestUrl, setManifestUrl] = React.useState(manifestURL);
+  const [canvasTime, setCanvasTime
+  ] = React.useState(startCanvasTime);
 
   const tabValues = [
-    { title: 'Details', ref: React.useRef(null) },
-    { title: 'Transcripts', ref: React.useRef(null) },
-    { title: 'Files', ref: React.useRef(null) },
-    { title: 'Markers', ref: React.useRef(null) },
+    { title: '詳細', ref: React.useRef(null) },
+    { title: '文字起こし', ref: React.useRef(null) },
+    { title: 'ファイル', ref: React.useRef(null) },
+    { title: 'マーカー', ref: React.useRef(null) },
   ];
 
   React.useEffect(() => {
@@ -35,24 +37,19 @@ const App = ({ manifestURL }) => {
   const handleUserInput = (e) => {
     setManifestUrl();
     setUserURL(e.target.value);
+    setCanvasTime(0);
   };
 
   return (
     <div className='ramp-demo'>
       <div className='ramp--details'>
         <h1>Ramp</h1>
-        <p>An interactive, IIIF powered A/V player built with components
-          from <a href="https://www.npmjs.com/package/@samvera/ramp"
-            target="_blank">
-            @samvera/ramp
-          </a> library. This player supports <em>IIIF Presentation 3.0 Manifests</em>. Please enter the URL
-          of your <em>public</em> manifest to view it in the player.
-        </p>
+        <p>IIIFを活用したインタラクティブなオーディオ/ビデオプレーヤーで、<a href="https://www.npmjs.com/package/@samvera/ramp" target="_blank">@samvera/ramp</a>ライブラリのコンポーネントを使用して構築されています。このプレーヤーは<em>IIIF Presentation 3.0マニフェスト</em>をサポートしています。プレーヤーで表示するための<em>公開</em>マニフェストのURLを入力してください。</p>
         <div className='ramp--form_container'>
           <form onSubmit={handleSubmit}>
             <div className='row'>
               <div className='col-1'>
-                <label htmlFor="manifesturl" className="ramp-demo__manifest-input-label">Manifest URL</label>
+                <label htmlFor="manifesturl" className="ramp-demo__manifest-input-label">マニフェストURL</label>
               </div>
               <div className='col-2'>
                 <input type='url'
@@ -60,9 +57,9 @@ const App = ({ manifestURL }) => {
                   name='manifesturl'
                   value={userURL}
                   onChange={handleUserInput}
-                  placeholder='Manifest URL'
+                  placeholder='マニフェストURL'
                   className="ramp-demo__manifest-input" />
-                <input type='submit' value='Set Manifest' className="ramp-demo__manifest-submit" />
+                <input type='submit' value='送信' className="ramp-demo__manifest-submit" />
               </div>
             </div>
           </form>
@@ -71,20 +68,23 @@ const App = ({ manifestURL }) => {
       <div className='ramp--player_container'>
         <IIIFPlayer
           manifestUrl={manifestUrl}
+          startCanvasTime={canvasTime}
         >
-          <div className="iiif-player-demo">
-            <MediaPlayer enableFileDownload={true} enablePlaybackRate={true} />
-            <div className="components-row">
-              <div className="nav">
+          <div style={{ display: 'flex', width: '100%' }}>
+            <div style={{ flex: 1, padding: '0 20px' }}>
+              <MediaPlayer enableFileDownload={true} enablePlaybackRate={true} />
+              <div style={{ padding: '20px 0' }}>
                 <AutoAdvanceToggle />
                 <StructuredNavigation />
               </div>
+            </div>
+            <div style={{ flex: 1, display: 'flex', flexDirection: 'column', padding: '0 20px' }}>
               <Tabs tabValues={tabValues} manifestUrl={manifestUrl} />
             </div>
           </div>
         </IIIFPlayer>
-      </div>
-    </div>
+      </div >
+    </div >
   );
 };
 
--- demo/index.js
+++ demo/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import config from './config';
+// import config from './config';
 
 const manifestURL = () => {
   const params = new URLSearchParams(window.location.search);
-  let url = `${config.url}/manifests/${config.env}/lunchroom_manners.json`;
+  // let url = `${config.url}/manifests/${config.env}/lunchroom_manners.json`;
+  let url = "https://nakamura196.github.io/ramp_data/rekion/3571280/manifest.json"
   if (params.has('iiif-content')) {
     url = params.get('iiif-content');
   }
   return url;
 };
 
+const startCanvasTime = () => {
+  const params = new URLSearchParams(window.location.search);
+  if (params.has('t')) {
+    return parseFloat(params.get('t'));
+  }
+  return 0;
+}
+
 ReactDOM.render(<App
-  manifestURL={manifestURL()}
+  manifestURL={manifestURL()} startCanvasTime={startCanvasTime()}
 />, document.getElementById('root'));
